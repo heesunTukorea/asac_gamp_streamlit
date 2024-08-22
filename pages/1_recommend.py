1da2c64f7d86127df492b785171438925bad5a15
heesun <jy911004@gmail.com>
2024-08-22 00:20:41 +0000

    as

--- pages/1_recommend.py
+++ pages/1_recommend.py
@@ -20,6 +20,7 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
             st.session_state.recommendations = []
             st.session_state.item_recommend_list = []
             st.session_state.review_recommend_list = []
+            st.session_state.hybrid_recommend_list = []
 
         if "selected_gmap2" not in st.session_state:
             st.session_state.selected_gmap2 = ""
@@ -35,7 +36,7 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
 
             # 랜덤 gmap_id1 선택 버튼 추가
             if st.sidebar.button("랜덤 선택"):
-                query = """SELECT gmap_id FROM `hive_metastore`.`silver`.`california_meta` ORDER BY RAND() LIMIT 1"""
+                query = """SELECT gmap_id1 FROM `hive_metastore`.`streamlit`.`gbdt_sample` ORDER BY RAND() LIMIT 1"""
                 cursor.execute(query)
                 gmap_id1 = cursor.fetchone()[0]
                 st.session_state.gmap_id1 = gmap_id1
@@ -44,52 +45,113 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
             if gmap_id1:
                 # 입력한 Gmap1에 대한 정보 조회
                 query =f"""
-                SELECT address,gmap_id,avg_rating,description,latitude,longitude,name,num_of_reviews,price,state,url,region,main_category 
-                FROM `hive_metastore`.`silver`.`california_meta` 
-                WHERE gmap_id = '{gmap_id1}'
+                SELECT address1,gmap_id1,avg_rating1,description1,latitude1,longitude1,name1,num_of_reviews1,price1,state1,url1,main_category1,first_main_category1,region1 
+                FROM `hive_metastore`.`streamlit`.`gmap_id1_info`
+                WHERE gmap_id1 = '{gmap_id1}'
                 
                 """
                 cursor.execute(query)
-                address,gmap_id,avg_rating,description,latitude,longitude,name,num_of_reviews,price,state,url,region,main_category = cursor.fetchone()
+                address1,gmap_id1,avg_rating1,description1,latitude1,longitude1,name1,num_of_reviews1,price1,state1,url1,main_category1,first_main_category1,region1  = cursor.fetchone()
 
                 
                 # text candidate 쿼리
-                # query = f"""
-                # SELECT gmap_id2, cosine_similarity
-                # FROM 'hive_metastore'.'item2item'.'cosine_candidate_rank'
-                # WHERE gmap_id1 = '{gmap_id1}'
-                # ORDER BY rank DESC
-                # LIMIT 5
-                # """
-                # 추천 결과 생성 리스트
-                item_recommend_list,review_recommend_list,recommendations = [],[],[]
                 
+                # 추천 결과 생성 리스트
+                item_recommend_list,hybrid_recommend_list,review_recommend_list,recommendations = [],[],[],[]
+                
+                #------------------------------------------------------------------------------------
+                #gbdt 쿼리
+                query = f"""
+                SELECT gmap_id2, prob
+                FROM `hive_metastore`.`streamlit`.`gbdt_sample`
+                WHERE gmap_id1 = '{gmap_id1}'
+                ORDER BY rank DESC
+                LIMIT 5
+                """
+                cursor.execute(query)
+                gbdt_gmap2_list = cursor.fetchall()
+                # gmap_id2 값만 추출
+                gmap_id2_values = [t[0] for t in gbdt_gmap2_list]
+                gbdt_prob = [t[1] for t in gbdt_gmap2_list]
+
+                # SQL 쿼리에 사용할 수 있도록 튜플 형태로 변환
+                gmap_id2_tuple = tuple(gmap_id2_values)
+
                 #item2item 쿼리         
                 query = f"""
-                SELECT address,gmap_id,avg_rating,latitude,longitude,name,main_category
-                FROM `hive_metastore`.`silver`.`california_meta`
-                ORDER BY RAND() LIMIT 5
+                SELECT address2,gmap_id2,avg_rating2,description2,latitude2,longitude2,name2,num_of_reviews2,price2,state2,url2,main_category2,first_main_category2,region2
+                FROM `hive_metastore`.`streamlit`.`gmap_id2_info`
+                WHERE gmap_id2 in {gmap_id2_tuple}
+                LIMIT 5
                 """
                 cursor.execute(query)
                 similar_items = cursor.fetchall()
                 item_recommend_list.extend(similar_items)
 
-
-                #텍스트 유사도 쿼리       
+                #--------------------------------------------------------------------------
+                #하이브리드 쿼리       
                 query = f"""
-                SELECT address,gmap_id,avg_rating,latitude,longitude,name,main_category
-                FROM `hive_metastore`.`silver`.`california_meta`
-                ORDER BY RAND() LIMIT 5
+                SELECT gmap_id2, prob
+                FROM `hive_metastore`.`streamlit`.`hybrid_sample`
+                WHERE gmap_id1 = '{gmap_id1}'
+                ORDER BY rank DESC
+                LIMIT 5
+                """
+                cursor.execute(query)
+                gbdt_gmap2_list = cursor.fetchall()
+                # gmap_id2 값만 추출
+                gmap_id2_values = [t[0] for t in gbdt_gmap2_list]
+                hybrid_prob = [t[1] for t in gbdt_gmap2_list]
+
+                # SQL 쿼리에 사용할 수 있도록 튜플 형태로 변환
+                gmap_id2_tuple = tuple(gmap_id2_values)
+
+                #gmap_id1 추출        
+                query = f"""
+                SELECT address2,gmap_id2,avg_rating2,description2,latitude2,longitude2,name2,num_of_reviews2,price2,state2,url2,main_category2,first_main_category2,region2
+                FROM `hive_metastore`.`streamlit`.`gmap_id2_info`
+                WHERE gmap_id2 in {gmap_id2_tuple}
+                LIMIT 5
+                """
+                cursor.execute(query)
+                similar_items = cursor.fetchall()
+                hybrid_recommend_list.extend(similar_items)
+                
+                #-----------------------------------------------------------------------------------
+                #리뷰 텍스트 쿼리       
+                query = f"""
+                SELECT gmap_id2, cosine_top4
+                FROM `hive_metastore`.`streamlit`.`text_sample`
+                WHERE gmap_id1 = '{gmap_id1}'
+                ORDER BY rank DESC
+                LIMIT 5
+                """
+                cursor.execute(query)
+                gbdt_gmap2_list = cursor.fetchall()
+                # gmap_id2 값만 추출
+                gmap_id2_values = [t[0] for t in gbdt_gmap2_list]
+                reveiw_prob = [t[1] for t in gbdt_gmap2_list]
+
+                # SQL 쿼리에 사용할 수 있도록 튜플 형태로 변환
+                gmap_id2_tuple = tuple(gmap_id2_values)
+
+                #gmap_id1 추출        
+                query = f"""
+                SELECT address2,gmap_id2,avg_rating2,description2,latitude2,longitude2,name2,num_of_reviews2,price2,state2,url2,main_category2,first_main_category2,region2
+                FROM `hive_metastore`.`streamlit`.`gmap_id2_info`
+                WHERE gmap_id2 in {gmap_id2_tuple}
+                LIMIT 5
                 """
                 cursor.execute(query)
                 similar_items = cursor.fetchall()
                 review_recommend_list.extend(similar_items)
                 
-                
+                total_prob = [gbdt_prob,hybrid_prob,reveiw_prob]
                 #세션 개선
                 st.session_state.item_recommend_list = item_recommend_list
                 st.session_state.review_recommend_list = review_recommend_list
-                recommendations = [st.session_state.item_recommend_list,st.session_state.review_recommend_list]
+                st.session_state.hybrid_recommend_list = hybrid_recommend_list
+                recommendations = [st.session_state.item_recommend_list,st.session_state.review_recommend_list,st.session_state.hybrid_recommend_list]
                 st.session_state.recommendations = recommendations
                 
                 # Streamlit에서 두 개의 열을 생성합니다. 첫 번째 열은 6의 가중치를, 두 번째 열은 4의 가중치를 가집니다.
@@ -105,13 +167,13 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
                         # 중앙의 열에서 작업을 수행합니다.
                         with col_main:
                             # 기본 맵 객체를 생성합니다. 지도의 중심을 지정된 위도와 경도로 설정하고, 확대 수준은 16으로 설정합니다.
-                            m = folium.Map(location=[latitude, longitude], zoom_start=16)
+                            m = folium.Map(location=[latitude1, longitude1], zoom_start=16)
 
                             # 빨간색 아이콘으로 마커를 추가합니다. 팝업과 툴팁을 설정하여 마커를 클릭하거나 호버할 때 정보를 표시합니다.
                             folium.Marker(
-                                [latitude, longitude],
-                                popup=f"<div style='text-align: left; color: black; font-size: 14px;'>{name}<br>{address}</div>",
-                                tooltip=f"{name}",
+                                [latitude1, longitude1],
+                                popup=f"<div style='text-align: left; color: black; font-size: 14px;'>{name1}<br>{address1}</div>",
+                                tooltip=f"{name1}",
                                 icon=folium.Icon(color='red')  # 마커의 아이콘을 빨간색으로 설정합니다.
                             ).add_to(m)
 
@@ -120,11 +182,13 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
                                 # 세션별로 다른 색상을 지정합니다.
                                 if j == 0:
                                     color_sel = 'green'
+                                elif j==1:
+                                    color_sel = 'orange'
                                 else:
                                     color_sel = 'blue'
                                 
                                 # 각 세션 내의 위치 정보를 순회하며 마커를 추가합니다.
-                                for i, (address,gmap_id,avg_rating,latitude,longitude,name,main_category) in enumerate(session_select):
+                                for i, (address,gmap_id,avg_rating,description,latitude,longitude,name,num_of_reviews,price,state,url,main_category,first_main_category,region) in enumerate(session_select):
                                     folium.Marker(
                                         [latitude, longitude],
                                         popup=f"<div style='text-align: left; color: black; font-size: 14px;'>{name}<br>{address}</div>",
@@ -173,23 +237,24 @@ with sql.connect(server_hostname=HOST, http_path=HTTP_PATH, access_token=PERSONA
                 #                 st.session_state.selected_gmap2 = gmap2
                 #                 change_page("메타정보")
                 #             display_image(img_resized, f"유사도: {cosine_similarity:.3f}")
+                title_list = ['**Item2Item Model**','**Hybrid Model**','**Review Simiarity**']
+                for idx,recommend_session in enumerate(recommendations):
+                    with st.expander(title_list[idx]):
+                        cols = st.columns(5)
+                        for i, (address,gmap_id,avg_rating,description,latitude,longitude,name,num_of_reviews,price,state,url,main_category,first_main_category,region) in enumerate(recommend_session):
+                            img_resized = resize_image(img_url)
 
-                with st.expander("**리뷰 유사도 결과**"):
-                    cols = st.columns(5)
-                    for i, (address,gmap_id,avg_rating,latitude,longitude,name,main_category) in enumerate(st.session_state.item_recommend_list[:5]):
-                        img_resized = resize_image(img_url)
-
-                        with cols[i % 5]:
-                            display_image(img_resized, f"")
-                            html_code = f"""
-                            <div style='text-align: center; color: gray; font-size: 18px;'>
-                                유사도: {100}%<br>
-                                Name: {name}<br>
-                                
-                            </div>
-                            """
-#Category: {main_category}
-                            st.markdown(html_code, unsafe_allow_html=True)
+                            with cols[i % 5]:
+                                display_image(img_resized, f"")
+                                html_code = f"""
+                                <div style='text-align: center; color: gray; font-size: 18px;'>
+                                    유사도: {round(float(total_prob[idx][i]),4)*100}%<br>
+                                    Name: {name}<br>
+                                    
+                                </div>
+                                """
+    #Category: {main_category}
+                                st.markdown(html_code, unsafe_allow_html=True)
                                 
 
 
--- pages/4_Chatbot.py
+++ pages/4_Chatbot.py
@@ -2,16 +2,30 @@ import streamlit as st
 import random
 import time
 from secrets_1 import *
+import os
+import sys
+# sys.path.append(os.environ["VECTOR_DB_PATH"])
+sys.path.append("home/ec2-user/workspace/workspace/vectorDB") 
+
+from retriever_module import Retriever
+
+review_retriever=Retriever("review")
+meta_retriever=Retriever("meta")
+
+m_test=meta_retriever.search("Please recommend a good park to visit with the family in California.")
+r_test=review_retriever.search("Please recommend a California restaurant with good service")
+
 
 # Streamed response emulator
 def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
+    # response = random.choice(
+    #     [
+    #         "Hello there! How can I assist you today?",
+    #         "Hi, human! Is there anything I can help you with?",
+    #         "Do you need help?",
+    #     ]
+    # )
+    response=[m_test,r_test]
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
